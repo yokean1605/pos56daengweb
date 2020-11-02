45089854f3a2340fe3a55a5a4802bc324e93b8e0
Yoke A. Yudistiro <yoke.mahardika@gmail.com>
2020-11-03 04:04:09 +0700

    add relationship to orders table

---
+++ database/migrations/2020_11_02_204227_add_relationships_to_orders_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRelationshipsToOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // customer id
+        Schema::table('orders', function (Blueprint $table) {
+            $table->integer('customer_id')->unsigned()->change();
+            $table->foreign('customer_id')->references('id')->on('customers')->onUpdate('cascade')->onDelete('cascade');
+       
+            // user id
+            $table->integer('user_id')->unsigned()->change();
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // customer id
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign('orders_customer_id_foreign');
+        });
+
+        Schema::table('orders', function(Blueprint $table){
+            $table->dropIndex('orders_customer_id_foreign');
+        });
+
+        Schema::table('orders', function(Blueprint $table){
+            $table->integer('customer_id')->change();
+        });
+
+        // user id
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign('orders_user_id_foreign');
+        });
+
+        Schema::table('orders', function(Blueprint $table){
+            $table->dropIndex('orders_user_id_foreign');
+        });
+
+        Schema::table('orders', function(Blueprint $table){
+            $table->integer('user_id')->change();
+        });
+    }
+}
---
+++ database/migrations/2020_11_02_210342_add_relationships_to_order_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRelationshipsToOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            //
+        });
+    }
+}
