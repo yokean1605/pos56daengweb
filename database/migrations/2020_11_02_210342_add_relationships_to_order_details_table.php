18a514f9b5966f34a8cb5c6fb22f8a0fe169c6db
Yoke A. Yudistiro <yoke.mahardika@gmail.com>
2020-11-03 04:17:21 +0700

    update relationships order_details

--- database/migrations/2020_11_02_210342_add_relationships_to_order_details_table.php
+++ database/migrations/2020_11_02_210342_add_relationships_to_order_details_table.php
@@ -14,7 +14,13 @@ class AddRelationshipsToOrderDetailsTable extends Migration
     public function up()
     {
         Schema::table('order_details', function (Blueprint $table) {
-            //
+            // order_id
+            $table->integer('order_id')->unsigned()->change();
+            $table->foreign('order_id')->references('id')->on('orders')->onUpdate('cascade')->onDelete('cascade');
+            
+            // product_id
+            $table->integer('product_id')->unsigned()->change();
+            $table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
@@ -25,8 +31,26 @@ class AddRelationshipsToOrderDetailsTable extends Migration
      */
     public function down()
     {
+        // order details
         Schema::table('order_details', function (Blueprint $table) {
-            //
+            $table->dropForeign('order_details_order_id_foreign');
+        });
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->dropIndex('order_details_order_id_foreign');
+        });
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->integer('order_id')->change();
+        });
+
+        // product_id
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->dropForeign('order_details_product_id_foreign');
+        });
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->dropIndex('order_details_product_id_foreign');
+        });
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->integer('product_id')->change();
         });
     }
 }
